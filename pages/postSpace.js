68b6d1618d97fcaa4904456840962fd4a617d05e
Thomas McMahon <glassoctopus@yahoo.com>
2024-03-09 14:59:34 -0600

    cleaned up console and got first step at getting a chain of posts

--- pages/postSpace.js
+++ pages/postSpace.js
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import { useAuth } from '../utils/context/authContext';
 // eslint-disable-next-line import/extensions
-import { getPosts } from '../api/postData';
+import { getPosts, getGhostPosts } from '../api/postData';
 import PostCard from '../components/PostCard';
+import GhostPostCard from '../components/GhostPostCard';
 
 function PostSpace() {
   const [posts, setPosts] = useState([]);
+  const [ghostPosts, setGhostPosts] = useState([]);
+
+  const chainOfPosts = [];
 
   // user ID using useAuth Hook
   const { user } = useAuth();
@@ -16,11 +20,33 @@ function PostSpace() {
     });
   };
 
+  const getAllTheGhostPosts = () => {
+    getGhostPosts(user.uid, true).then((GhostPostsData) => {
+      setGhostPosts(GhostPostsData);
+    });
+  };
+
   // make the call to the API to get all the Posts on component render
   useEffect(() => {
     getAllThePosts();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    getAllTheGhostPosts();
   }, []);
+
+  const getGhostsOfParent = (parentPost, ghostPostsArray) => {
+    const currentParentPostId = parentPost.postId;
+    let postIdIndex = '';
+    let postTempStorage = [];
+    let newParentPost = {};
+    if (ghostPostsArray.find((object) => Object.values(object).includes(currentParentPostId))) {
+      postIdIndex = ghostPostsArray.findIndex((object) => Object.values(object).includes(currentParentPostId));
+      newParentPost = ghostPostsArray[postIdIndex];
+      postTempStorage = ghostPostsArray.splice(postIdIndex);
+      chainOfPosts.push(...postTempStorage);
+      console.warn('newParentPost', newParentPost, 'ghostPostsArray length', ghostPostsArray.length, 'chainOfPosts', chainOfPosts);
+      return getGhostsOfParent(newParentPost, ghostPostsArray);
+    }
+    return chainOfPosts;
+  };
 
   return (
     <div
@@ -43,10 +69,30 @@ function PostSpace() {
       </Link> */}
         <h2>{posts ? 'Posts loaded' : 'No Posts'}</h2>
         <h3>{}</h3>
-        {/* TODO: map over post  */}
-        {posts.map((post) => (
-          <PostCard key={post.postId} postObj={{ ...post, isGhost: post.isGhost === 'true' }} onUpdate={getAllThePosts} />
-        ))}
+        {/* map over post, but only show one, STILL working on */}
+        {posts.map((post) => {
+          if (ghostPosts.find((object) => Object.values(object).includes(post.postId))) {
+            chainOfPosts.push(post);
+            getGhostsOfParent(post, ghostPosts);
+          }
+          return (
+            <PostCard key={post.postId} postObj={{ ...post }} onUpdate={getAllThePosts} />
+          );
+        })}
+        <h2>{ghostPosts ? 'Ghost Posts loaded' : 'No Ghost Posts'}</h2>
+        <h3>{}</h3>
+        <div
+          className="d-flex flex-nowrap"
+          style={{
+            display: 'flex', flexWrap: 'nowrap', overflowX: 'auto', maxHeight: '70vh', // Limit the height to allow vertical scrolling
+          }}
+        >
+          {ghostPosts.map((ghostPost) => (
+            <div key={ghostPost.postId} style={{ marginRight: '10px' }}>
+              <GhostPostCard postObj={{ ...ghostPost }} onUpdate={getAllTheGhostPosts} />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
